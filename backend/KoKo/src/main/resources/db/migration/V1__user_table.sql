2c43cbdd80fde15ea796d09aa8ffb58c78de013f
krapovskik <krapovskik@gmail.com>
2022-03-29 14:51:17 +0200

    user table, db config

--- backend/KoKo/src/main/resources/db/migration/V1__user_table.sql
+++ backend/KoKo/src/main/resources/db/migration/V1__user_table.sql
@@ -1,9 +1,10 @@
 create table app_users(
 
-    app_user_id bigserial primary key,
+    id bigserial primary key,
     first_name text not null,
     last_name text not null,
     email text not null,
-    is_valid boolean default false,
+    password text not null,
+    is_valid boolean default false
 
-)
+);
