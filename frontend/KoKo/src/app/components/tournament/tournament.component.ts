1902a10a31c7b0eb436ea9fc883d4374a8fd3bfa
Kosta Fortumanov <kosta.fortumanov@sorsix.com>
2022-04-14 10:38:45 +0200

    tournament brackets

--- frontend/KoKo/src/app/components/tournament/tournament.component.ts
+++ frontend/KoKo/src/app/components/tournament/tournament.component.ts
@@ -1,13 +1,37 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {RegisterUserDialog} from "../header/registerUserDialog/register-user-dialog.component";
+import {TournamentService} from "../../service/tournament.service";
 
 @Component({
     selector: 'app-tournament',
     templateUrl: './tournament.component.html',
     styleUrls: ['./tournament.component.css']
 })
-export class TournamentComponent {
+export class TournamentComponent implements OnInit {
 
     sideBarOpen = false
+    data = this.tournamentService.getTournamentBracket(0)
+
+    constructor(
+        private dialog: MatDialog,
+        private tournamentService: TournamentService
+    ) {
+    }
+
+    ngOnInit(): void {
+        // @ts-ignore
+        window.bracketsViewer.render(this.data)
+
+        // @ts-ignore
+        window.bracketsViewer.onMatchClicked = (match) => {
+
+            console.log(match)
+            this.dialog.open(RegisterUserDialog)
+            // @ts-ignore
+            // window.bracketsViewer.updateMatch(match);
+        };
+    }
 
     sideBarToggle() {
         this.sideBarOpen = !this.sideBarOpen
